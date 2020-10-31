b3dd12ba13ba5d4675fa9ddb9dfc77e71fad9e85
bigbaldbad <lazarev.nikitushk4@yandex.ru>
2020-10-31 14:53:33 +0300

    Добавьте файлы проекта.

---
+++ pr_11_systprog/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_11_systprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа: ");
+            int[] array = new int[5];
+
+            for (int i = 0; i < 5; i++)
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Максимальное значение:  " + Max(array).ToString());
+            Console.ReadLine();
+        }
+        static int Max(int[] array)
+        {
+            int max;
+            return max = array.Max();
+        }
+    }
+}
